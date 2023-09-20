82553b6788e15103a6f337093f5ba2aace978bdf
Iamneo <techadmin@iamneo.ai>
2023-09-20 10:41:39 +0000

    Backup Commit

---
+++ reactapp/src/App.js
@@ -0,0 +1,13 @@
+import Quiz from "./quiz/Quiz";
+// import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Quiz />
+    </div>
+  );
+}
+
+export default App;
